cf25f89ea22d83ce4bc7449bcf42409516cf0a02
ivan <ivanferrari95.if@gmail.com>
2021-03-31 19:35:24 +0200

    functions embedded (before ROC)

--- R/package_draft.R
+++ R/package_draft.R
@@ -5,6 +5,8 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 library(gtools)
+library(ROCR)
+library(stringr)
 
 ## Essential functions:
 
@@ -16,23 +18,20 @@ load <- function(data,  header = TRUE, sep = ";", na.strings="" ) {
   return(CombiROC_data)
 }
 
-data <- load("data/demo_5Ags.csv")
+
+
 
 
 # - a function transform the data in long format for plotting purposes ?
 
 ## long format
-data_long <- tidyr::pivot_longer(data, cols =  starts_with("Marker"), names_to = "Markers", values_to = "Values")
+CombiROC_long <- function(CombiROC_data){
+  data_long <- tidyr::pivot_longer(data, cols =  3:dim(data)[2], names_to = "Markers", values_to = "Values")
 
-## list of long data tables (one for each class)
-nclass <- unique(data[,2])
-long_data_class_list <-c()
-for (class in nclass) {long_data_class_list <- 
-  list(long_data_class_list, dplyr::filter(data_long, Class == class))}
+  
+  return(data_long)
+}
 
-long_data_class_list[[1]]<- long_data_class_list[[1]][[2]] # to remove the first NULL element
-
-names(long_data_class_list) <- nclass # to rename elements
 
 
 
@@ -40,106 +39,115 @@ names(long_data_class_list) <- nclass # to rename elements
 #   plot (MAD to get rid of outliers?) and a corresponding dataframe object 
 #   (maybe with a message to aknowledge the user of its creation)
 
-
-ggplot(data = data_long, aes(Markers, Values)) +
-  geom_boxplot(aes(color = Class)) +
-  theme_classic()  # ADD SUPERIOR LIMIT ?
-
-for (class in nclass) {print(paste("STATISTICS OF CLASS ", class, ":", sep = ""))
-                       print(summary(data.frame(long_data_class_list[class])[,4]))}
+markers_overview <- function(data_long, ylim=NULL){
+  
+  nclass <- unique(data_long$Class)
+  
+  
+  for (class in nclass){print(paste("STATISTICS OF CLASS ", class, ":", sep = ""))
+    print(summary(data_long[data_long$Class==class, 4]))}
+  
+  ggplot(data_long, aes(Markers, Values)) +
+    geom_boxplot(aes(color = Class)) +
+    theme_classic()+
+    coord_cartesian(ylim = c(0,ylim))}
 
 # - a function to perform the COMBINATORIAL ANALYSIS
-dfe <- data[data$Class== nclass[1],]
-dfe<- dfe[,3:dim( dfe)[2]]
-dfe<-t(dfe)
 
+Combi <-function(data,signalthr=0, combithr=1){
+
+nclass <- unique(data$Class)
+
+#sample df to get names and dims
+dfe <- data[data$Class== nclass[1], 3:dim(data)[2]]
+dfe<-t(dfe)
 n_features<-length(rownames(dfe))
+
+# parameters for combinations
 k<-1:n_features
 K<-2^n_features-1   
 
-
-
-df_comb_list <- c()
-for (i in nclass) { df <- data[data$Class== i,]
-  df <- df[,3:dim( df)[2]]
-  df <- t(df)
-  df_comb_list <- list(df_comb_list, df)}
-df_comb_list[[1]]<- df_comb_list[[1]][[2]] # to remove the first NULL element
-
-names(df_comb_list) <- nclass # to rename elements
-
-
-
 ### list of all possible combinations
-listCombinationAntigens <- array(0,dim=c(K,1))
+listCombinationMarkers <- array(0,dim=c(K,1))
 
 ### relative frequency for each class  (the row numbers depend on the K possible combinations while the column numbers depends on the number classes:2 (classA2 and classB2, pairwise comparison))
-frequencyCombinationAntigens<-array(0,dim=c(K,2))
+frequencyCombinationMarkers<-array(0,dim=c(K,2))
 
-
-
-signalthr<- 450
-combithr <- 1
-
+# COMPUTING COMBINATIONS AND FREQUENCIES
 index<-1
-
-
 
 for (i in 1:length(k)){
   temp<- combinations(n_features,k[i],rownames(dfe))
-  # storage the row index to calculate the relative frequency
+  # storing the row index to calculate the relative frequency
   row_index_combination<-combinations(n_features,k[i],k)
   for (j in 1:dim(temp)[1]){
-    listCombinationAntigens[index,1]<-paste(temp[j,],collapse="-")
+    listCombinationMarkers[index,1]<-paste(temp[j,],collapse="-")
     ## single antigen
     if(dim(temp)[2]==1){ ## 1 antigen combination
       for (n in 1:length(nclass)){
-         frequencyCombinationAntigens[index,n]<-length(which(
-           data.frame(df_comb_list[n])[row_index_combination[j,],]>=signalthr))    #input$signalthr
+         frequencyCombinationMarkers[index,n]<-length(which(
+           t(data[data$Class== nclass[n], 3:dim(data)[2]])[row_index_combination[j,],]>=signalthr))    #input$signalthr
       }
     }else{ ## more than 1 antigen (combination)
       for (n in 1:length(nclass)){
-         frequencyCombinationAntigens[index,n]<- length(which((colSums(
-           data.frame(df_comb_list[n])[row_index_combination[j,],]>=signalthr))>=combithr))   #input$signalthr))>=input$combithr
+         frequencyCombinationMarkers[index,n]<- length(which((colSums(
+           t(data[data$Class== nclass[n], 3:dim(data)[2]])[row_index_combination[j,],]>=signalthr))>=combithr))   #input$signalthr))>=input$combithr
      }}
     index<-index+1
   }
 }
 
-
-# - a function to SHOW the BEST COMBINATIONS (dataframe + buble chart?):
-
-### Sensitivity  (the row numbers depend on the K possible combinations while the column numbers depends on the number classes:2 (classA2 and classB2, pairwise comparison))
-
+# COMPUTING SE AND SP FOR EACH COMB. AND FOR BOTH CLASSES
 
 names<- c()
 
-SE_SP<-array(0,dim=c(K,length(nclass)*2))
+SE_SP<-array(0,dim=c(K,2*2))
 
 for (i in  1:length(nclass)){
-  SE_SP[,i*2-1]<- round(frequencyCombinationAntigens[,1]*100/length(colnames((data.frame(df_comb_list[i]))))
+  SE_SP[,i]<- round(frequencyCombinationMarkers[,i]*100/length(colnames(t(data[data$Class==nclass[i],])))
                       ,digits=0)
-  names[i*2-1] <- paste('SE%', nclass[i])
-  SE_SP[,i*2]<- 100-SE_SP[,i*2-1]
-  names[i*2] <- paste('SP%', nclass[i])
+  names[i] <- paste('SE%', nclass[i])
+  SE_SP[,i+2]<- 100-SE_SP[,i]
+  names[i+2] <- paste('SP%', nclass[i])
 }
 
 SE_SP <- data.frame(SE_SP)
-rownames(SE_SP)<-listCombinationAntigens
+rownames(SE_SP)<-listCombinationMarkers
 colnames(SE_SP)<- names
-SE_SP$min <-  apply(SE_SP, 1, FUN=min)
-ranked_SE_SP<-SE_SP[order(-SE_SP$min),] # 9/14
+
+return(SE_SP)
+}
 
 
-#   ALTERNATIVE 1 - Do something like the app, where you select SN and SP 
-#                   and the app returns the buble chart and the table
-#   ALTERNATIVE 2 - The function returns the table with combinations 
-#                   ranked by F1-score, showing on the top the 
-#                   combinations with the highest SN and SP (I suppose)
-#                   in order not to force a priori threshold selection.
+
+#The function returns the table with combinations ranked by F1-score
+
+ranked_combs <- function(combo_table){
+
+combo_table$F1<-  2*(combo_table[,1] * combo_table[,4])/(combo_table[,1] + combo_table[,4])
+
+
+ranked_SE_SP<-combo_table[order(-combo_table$F1), ]  # REMEMBER TO ADD FREQUENCY
+
+ranked_SE_SP<- ranked_SE_SP[,c(1,4,5),]
+
+return(ranked_SE_SP)}
 
 
 # - a function to SHOW ROC CURVES and corresponding METRICS of the 
 #   selected combinations
 
 # - a function to ASSESS THE PERFORMANCES?
+
+
+
+
+
+
+### WORKFLOW TEST ###
+
+data <- load("data/demo_5Ags.csv")
+data_long <- CombiROC_long(data)
+markers_overview(data_long, ylim =2000)
+mks <-Combi(data, signalthr = 450, combithr = 1)
+rmks<- ranked_combs(mks)
