eebd90a282126bf097d3803d3f2ec911ee9f4383
ivan <ivanferrari95.if@gmail.com>
2021-05-21 13:41:42 +0200

    Datasets man updated

--- R/demo_data.R
+++ R/demo_data.R
@@ -1,6 +1,6 @@
 #' Combiroc built-in demo data (proteomics data from Zingaretti et al. 2012 - PMC3518104)
 #'
-#' A dataset containing signal intensity values of a 5-marker signatures for Autoimmune Hepatitis (AIH
+#' A dataset containing signal intensity values of a 5-marker signatures for Autoimmune Hepatitis (AIH).
 #' Samples have been clinically diagnosed as “abnormal” (class A) or "normal" (class B).
 #'
 #'
--- R/demo_unclassified_data.R
+++ R/demo_unclassified_data.R
@@ -1,6 +1,6 @@
 #' Combiroc built-in unclassified demo data
 #'
-#' A dataset containing signal intensity values of a 5-marker signatures for Autoimmune Hepatitis (AIH
+#' A dataset containing signal intensity values of a 5-marker signatures for Autoimmune Hepatitis (AIH).
 #' This dataset has been obtained by randomly picking 20 samples from demo_data without their classification.
 #'
 #'
