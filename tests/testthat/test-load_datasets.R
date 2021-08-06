e176aedcb8c92c75ba1bade500f3b9f39e7e84b5
Ric <rossi@ingm.org>
2021-08-06 13:55:40 +0200

    uploaded demo data for test & modified test path

--- tests/testthat/test-load_datasets.R
+++ tests/testthat/test-load_datasets.R
@@ -1,6 +1,5 @@
 test_that("A dataframe is read", {
-  path <- file.path(system.file("data", package="combiroc"), "demo_data.csv")
-  d <- load_data(data = path)
+  d <- load_data(data = "demo_data.csv")
   expect_s3_class(d, "data.frame")
 })
 
