61debc61021f644a3e9cd43ebad19f395ddb0dd9
ricrossi <ric.rossi@gmail.com>
2021-08-09 12:29:19 +0200

    simplified test to use setuptest file

--- tests/testthat/test-combiroc_long.R
+++ tests/testthat/test-combiroc_long.R
@@ -1,5 +1,3 @@
 test_that("combi_long returns a tibble", {
-  d <- load_data(data = "demo_data.csv")
-  d <- combiroc_long(d)
-  expect_s3_class(d, c("tbl_df", "tbl", "data.frame"))
+  expect_s3_class(data_long, c("tbl_df", "tbl", "data.frame"))
 })
