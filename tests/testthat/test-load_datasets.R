a2a41495ed8e244ade5cd5816cee522403333d3a
ricrossi <ric.rossi@gmail.com>
2021-08-09 09:44:41 +0200

    extended load_data test

--- tests/testthat/test-load_datasets.R
+++ tests/testthat/test-load_datasets.R
@@ -1,6 +1,9 @@
+d <- load_data(data = "demo_data.csv")
+
 test_that("A dataframe is read", {
-  d <- load_data(data = "demo_data.csv")
   expect_s3_class(d, "data.frame")
 })
 
-
+test_that("the name of second column is enforced to Class", {
+  expect_setequal(colnames(d)[2], "Class")
+})
