3689a46aab120c0375670d1d0375a850c423d1c1
ricrossi <ric.rossi@gmail.com>
2021-08-09 12:23:06 +0200

    modified to use testsetup file

--- tests/testthat/test-markers_distribution.R
+++ tests/testthat/test-markers_distribution.R
@@ -1,21 +1,17 @@
-d <- load_data(data = "demo_data.csv")
-d <- combiroc_long(d)
-d <- markers_distribution(d, case_class = "A")
-
 test_that("markers_ditribution generates a list of length 5", {
-  expect_type(d, "list")
-  expect_length(d, 5)
+  expect_type(distr, "list")
+  expect_length(distr, 5)
 })
 
 test_that("graphical objects are ggplot objects", {
-  expect_s3_class(d$Boxplot, c("gg", "ggplot"))
-  expect_s3_class(d$Density_plot, c("gg", "ggplot"))
-  expect_s3_class(d$ROC, c("gg", "ggplot"))
+  expect_s3_class(distr$Boxplot, c("gg", "ggplot"))
+  expect_s3_class(distr$Density_plot, c("gg", "ggplot"))
+  expect_s3_class(distr$ROC, c("gg", "ggplot"))
 })
 
 test_that("table objects are data frames", {
-  expect_s3_class(d$Coord, "data.frame")
-  expect_s3_class(d$Density_summary, "data.frame")
+  expect_s3_class(distr$Coord, "data.frame")
+  expect_s3_class(distr$Density_summary, "data.frame")
 })
 
 
