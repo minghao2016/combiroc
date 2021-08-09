040f42021157792ff3cfd985babbaaf44d4ac758
ricrossi <ric.rossi@gmail.com>
2021-08-09 11:35:38 +0200

    typo in test description

--- tests/testthat/test-markers_distribution.R
+++ tests/testthat/test-markers_distribution.R
@@ -7,7 +7,7 @@ test_that("markers_ditribution generates a list of length 5", {
   expect_length(d, 5)
 })
 
-test_that("graphical obj objects are ggplot objects", {
+test_that("graphical objects are ggplot objects", {
   expect_s3_class(d$Boxplot, c("gg", "ggplot"))
   expect_s3_class(d$Density_plot, c("gg", "ggplot"))
   expect_s3_class(d$ROC, c("gg", "ggplot"))
@@ -17,6 +17,8 @@ test_that("table objects are data frames", {
   expect_s3_class(d$Coord, "data.frame")
   expect_s3_class(d$Density_summary, "data.frame")
 })
+
+
 
 
 
