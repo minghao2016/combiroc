5960cf1d05be8f598a661a7b8e6eb9c5fdace912
ricrossi <ric.rossi@gmail.com>
2021-08-09 15:28:49 +0200

    updated testsetup with reports object

--- tests/testthat/setuptests_combiroc.R
+++ tests/testthat/setuptests_combiroc.R
@@ -6,3 +6,5 @@ distr <- markers_distribution(data_long, case_class = "A")
 tab <- combi(data, signalthr = 450, combithr = 1)
 mks <- se_sp(data, tab)
 rmks <- ranked_combs(data, mks, case_class = 'A', min_SE = 40, min_SP = 80)
+reports <-roc_reports(data, markers_table = tab, case_class = 'A',
+                      single_markers =c('Marker1'), selected_combinations = c(11,15))
