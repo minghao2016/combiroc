a8c89fb9555104b6d31bc37d0618caf85873144d
ricrossi-ingm <rossi@ingm.org>
2021-08-12 13:24:25 +0200

    [skip travis] added object for show_markers test

--- tests/testthat/setuptests_combiroc.R
+++ tests/testthat/setuptests_combiroc.R
@@ -8,7 +8,7 @@ mks <- se_sp(data, tab)
 rmks <- ranked_combs(data, mks, case_class = 'A', min_SE = 40, min_SP = 80)
 reports <-roc_reports(data, markers_table = tab, case_class = 'A',
                       single_markers =c('Marker1'), selected_combinations = c(11,15))
-show_markers(selected_combinations =c(11,15), markers_table = tab)
+sh_mk <- show_markers(selected_combinations =c(11,15), markers_table = tab)
 combs_list <- combs_with(markers=c('Marker1', 'Marker3'), markers_table = tab)
 unc_data <- load_unclassified_data(data = "demo_unclassified_data.csv", sep = ',')
 cl_data <- classify(unc_data, Models =  reports$Models, Metrics = reports$Metrics, Positive_class = "affected", Negative_class = "healthy")
